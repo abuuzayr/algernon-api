272a264c9b02d8f7dd07174e1f66c85829b09e04
Hellyna NG <hellyna@hellyna.com>
2017-10-31 16:53:42 +0800

    Populate more data before starting the server.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import mongoose from './services/mongoose'
 import express from './services/express'
 import api from './api'
 import { User } from './api/user'
+import { SalesChannel } from './api/sales-channel'
 
 const app = express(api)
 const server = http.createServer(app)
@@ -13,20 +14,69 @@ mongoose.connect(
   // Bug https://github.com/Automattic/mongoose/issues/5399
   { useMongoClient: true, promiseLibrary: global.Promise }
 ).then(function () {
-  const adminEmail = 'admin@example.com'
-  User.findOne({ email: adminEmail }, function (err, user) {
-    if (err) {
-      console.error(err)
-      return
-    }
+  const adminEmail = 'super_admin@example.com'
+  const storeAdmin1Email = 'store_admin1@example.com'
+  const storeAdmin2Email = 'store_admin2@example.com'
+  const sa1domain = 'storeadmin1.example.com'
+  const sa2domain = 'storeadmin2.example.com'
 
-    if (user) return
+  Promise.all([
+    User.findOne({ email: adminEmail }).exec().then((user) => {
+      if (user) return user
 
-    (new User({
-      email: adminEmail,
-      password: 'adminadmin',
-      role: 'super_admin'
-    })).save()
+      user = new User({
+        email: adminEmail,
+        password: 'adminadmin',
+        role: 'super_admin'
+      })
+      return user.save()
+    }),
+    User.findOne({ email: storeAdmin1Email }).exec().then((user) => {
+      if (user) return user
+
+      user = new User({
+        email: storeAdmin1Email,
+        password: 'adminadmin',
+        role: 'store_admin'
+      })
+
+      return user.save()
+    }),
+    User.findOne({ email: storeAdmin2Email }).exec().then((user) => {
+      if (user) return user
+
+      user = new User({
+        email: storeAdmin2Email,
+        password: 'adminadmin',
+        role: 'store_admin'
+      })
+      return user.save()
+    })
+  ]).then(([superAdmin, storeAdmin1, storeAdmin2]) => {
+    Promise.all([
+      SalesChannel.findOne({ domain: sa1domain }).exec().then((sc) => {
+        if (sc) return
+
+        return (new SalesChannel({
+          userRef: storeAdmin1.id,
+          domain: sa1domain,
+          type: 'ecommerce',
+          name: 'Store Admin 1 Ecommerce'
+        })).save()
+      }),
+      SalesChannel.findOne({ domain: sa2domain }).exec().then((sc) => {
+        if (sc) return
+
+        return (new SalesChannel({
+          userRef: storeAdmin2.id,
+          domain: sa2domain,
+          type: 'ecommerce',
+          name: 'Store Admin 2 Ecommerce'
+        })).save()
+      })
+    ]).catch((err) => {
+      if (err) throw err
+    })
   })
 })
 
